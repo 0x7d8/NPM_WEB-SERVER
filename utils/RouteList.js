9020d3807f9768e44fe98d91ef0b8d5df5571197
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-11-01 14:22:32 +0100

    [0.0.3] #0 - CHECK FOR VALID TYPE AND FIX README

--- utils/RouteList.js
+++ utils/RouteList.js
@@ -1,9 +1,14 @@
+const types = [
+    'GET'
+]
+
 class RouteList {
     constructor() {
         this.urls = {}
     }
 
     set(type, url, code) {
+        if (!types.includes(type)) throw TypeError('No Valid Request Type: ' + type)
         this.urls[url] = { type, code }
     }; list() {
         return this.urls
