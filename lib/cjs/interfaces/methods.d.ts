0f7f5a79a4c4974d57aedb755a4e198defbd1449
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2023-02-11 13:19:37 +0100

    [3.0.1] #0 - UPDATE README

--- lib/cjs/interfaces/methods.d.ts
+++ lib/cjs/interfaces/methods.d.ts
@@ -3,9 +3,10 @@ declare enum HTTPtypes {
     /** HTTP Request Type */ delete = "DELETE",
     /** HTTP Request Type */ patch = "PATCH",
     /** HTTP Request Type */ post = "POST",
+    /** HTTP Request Type */ head = "HEAD",
     /** HTTP Request Type */ put = "PUT",
     /** HTTP Request Type */ get = "GET",
     /** Internal Request Type */ static = "STATIC"
 }
 export default HTTPtypes;
-export type Types = 'OPTIONS' | 'DELETE' | 'PATCH' | 'POST' | 'PUT' | 'GET' | 'STATIC';
+export type Types = 'OPTIONS' | 'DELETE' | 'PATCH' | 'POST' | 'HEAD' | 'PUT' | 'GET' | 'STATIC';
--- lib/esm/interfaces/ctr.d.ts
+++ lib/esm/interfaces/ctr.d.ts
@@ -33,7 +33,7 @@ interface printFileOptions {
      * @default false
     */ cache?: boolean;
 }
-export default interface ctr<Custom = any, HasError = false, Body = any> {
+export default interface Ctr<Custom = any, HasError = false, Body = any> {
     /** The Server Controller Class */ controller: ServerController;
     /** A Collection of all Headers */ readonly headers: valueCollection<Lowercase<string>, string>;
     /** A Collection of all Cookies */ readonly cookies: valueCollection<string, string>;
@@ -53,12 +53,12 @@ export default interface ctr<Custom = any, HasError = false, Body = any> {
     /** The Raw HTTP Server Req Variable */ rawReq: IncomingMessage;
     /** The Raw HTTP Server Res Variable */ rawRes: ServerResponse;
     /** The Error from the Request */ error?: HasError extends true ? Error : undefined;
-    /** Set an HTTP Header to add */ setHeader: (name: string, value: string | number) => ctr;
-    /** Set a Custom Variable */ setCustom: <Type extends keyof Custom>(name: Type, value: Custom[Type]) => ctr;
-    /** The Request Status to Send */ status: (code: number) => ctr;
-    /** Redirect a Client to another URL */ redirect: (location: string, statusCode?: 301 | 302) => ctr;
-    /** Print a Message to the Client (automatically Formatted) */ print: (msg: any, options?: printOptions) => ctr;
-    /** Print the Content of a File to the Client */ printFile: (path: string, options?: printFileOptions) => ctr;
+    /** Set an HTTP Header to add */ setHeader: (name: string, value: string | number) => Ctr;
+    /** Set a Custom Variable */ setCustom: <Type extends keyof Custom>(name: Type, value: Custom[Type]) => Ctr;
+    /** The Request Status to Send */ status: (code: number) => Ctr;
+    /** Redirect a Client to another URL */ redirect: (location: string, statusCode?: 301 | 302) => Ctr;
+    /** Print a Message to the Client (automatically Formatted) */ print: (msg: any, options?: printOptions) => Ctr;
+    /** Print the Content of a File to the Client */ printFile: (path: string, options?: printFileOptions) => Ctr;
     /** Custom Variables that are Global */ '@': Custom;
 }
 export {};
