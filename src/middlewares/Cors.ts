fc95280f3c5d062794bd345f0863ca08a52802d5
Robert Jansen <rjansengd@gmail.com>
2024-11-16 23:49:37 +0000

    cors middleware set default origin

--- src/middlewares/Cors.ts
+++ src/middlewares/Cors.ts
@@ -33,6 +33,8 @@ export default new Middleware<{
 					break
 				}
 			}
+
+			ctr.headers.set('access-control-allow-origin', config.origins.at(-1))
 		}
 
 		if (ctr.url.method === 'OPTIONS' && !context.route) {
